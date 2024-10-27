2429d4592f6b0f29ad48f46cc81d8eb8946fcbda
dvh-patcher <danial142001@gmail.com>
2024-10-28 04:35:59 +0800

    Create ttl64.sh

---
+++ ttl64.sh
@@ -0,0 +1,8 @@
+echo -e "BYPASS-TTL64"
+wget -q -O /etc/init.d/firewall-custom "https://github.com/dvh-patcher/system/raw/refs/heads/main/firewall-custom";
+chmod 755 /etc/init.d/firewall-custom;
+
+/etc/init.d/firewall-custom enable
+/etc/init.d/firewall-custom start
+
+echo -e "FINISH BYPASS CAN CHECK PING"
