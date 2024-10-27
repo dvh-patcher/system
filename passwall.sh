0226746e4150d25b9d6db273610a2f3f0f82b4df
dvh-patcher <danial142001@gmail.com>
2024-10-28 04:36:29 +0800

    Create passwall.sh

---
+++ passwall.sh
@@ -0,0 +1,11 @@
+echo -e "INSTALL-XRAYMOD"
+wget -q "https://github.com/dvh-patcher/system/raw/refs/heads/main/xray-core_1.7.2-1_aarch64_cortex-a53.ipk";
+opkg install xray-core_1.7.2-1_aarch64_cortex-a53.ipk;
+
+echo -e "INSTALL-PASSWALL"
+wget -q "https://github.com/dvh-patcher/system/raw/refs/heads/main/luci-app-passwall_4.66-8_all.ipk";
+opkg install luci-app-passwall_4.66-8_all.ipk;
+uci commit
+
+rm -rf /root/*
+echo -e "FINISH INSTALL CAN CLOSE TERMINAL"
